dc48dbb7ab44d70550c77bb9b64f28061205773b
Shreyam Adhikari <shreyam1008@gmail.com>
2017-12-10 23:23:45 +0545

    models for restaurants

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+import datetime
+
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50)
+    city = models.CharField(max_length=50, default="Kathmandu", blank=False)
+    location = models.CharField(max_length=50, blank=True)
+    open_time = models.TimeField(default=datetime.time(9, 00))
+    close_time = models.TimeField(default=datetime.time(18, 00))
+
+    def __str__(self):
+        return self.name
