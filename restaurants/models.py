05a407f3517637989f3d9e6e8fecf0af15a952f3
Shreyam Adhikari <shreyam1008@gmail.com>
2017-12-11 01:42:24 +0545

    owner(User instance) for models.restaurants

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 from django.db.models.signals import pre_save
+from django.conf import settings
 import datetime
 
 
 from .utils import unique_slug_generator
 
-
-
+User = settings.AUTH_USER_MODEL
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=50)
-    city = models.CharField(max_length=50, default="Kathmandu", blank=False)
-    location = models.CharField(max_length=50, blank=True)
-    open_time = models.TimeField(default=datetime.time(9, 00))
-    close_time = models.TimeField(default=datetime.time(18, 00))
-    timestamp = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    owner =         models.ForeignKey(User, on_delete=models.CASCADE)
+    name =          models.CharField(max_length=50)
+    city =          models.CharField(max_length=50, default="Kathmandu", blank=False)
+    location =      models.CharField(max_length=50, blank=True)
+    open_time =     models.TimeField(default=datetime.time(9, 00))
+    close_time =    models.TimeField(default=datetime.time(18, 00))
+    timestamp =     models.DateTimeField(auto_now_add=True)
+    updated =       models.DateTimeField(auto_now=True)
 
     slug = models.SlugField(null=True, blank=True)
 
