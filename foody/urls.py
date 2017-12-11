576f949f02807b3662f35a273eace81ce85f5a26
Shreyam Adhikari <shreyam1008@gmail.com>
2017-12-12 01:33:51 +0545

    created food app

--- foody/urls.py
+++ foody/urls.py
@@ -9,17 +9,21 @@ from django.views.generic.base import TemplateView
 
 #access templates with help of views
 from restaurants.views import restaurant_list, restaurant_detail, restaurant_create
+from foods.views import foods_list, foods_detail, foods_create
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="home.html"), name="home"),
+
     path('restaurants/', restaurant_list, name='restaurants'),
-    path('restaurants/create', restaurant_create, name='create'),
-    path('restaurants/<slug>', restaurant_detail, name='detail'),
+    path('restaurants/create/', restaurant_create, name='create'),
+    path('restaurants/<slug>/', restaurant_detail, name='detail'),
+
+    path('foods/', food_list, name='foods'),
 
     path('reviews/', TemplateView.as_view(template_name="temp.html"), name='reviews'),
-    path('foods/', TemplateView.as_view(template_name="temp.html"), name='foods'),
     path('profile/', TemplateView.as_view(template_name="temp.html"), name='profile'),
     path('logout/', TemplateView.as_view(template_name="temp.html"), name='logout'),
     path('login/', TemplateView.as_view(template_name="temp.html"), name='login'),
