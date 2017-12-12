c92a51a59b45d4a79329336635926d4d4b5d39bf
Shreyam Adhikari <shreyam1008@gmail.com>
2017-12-13 00:33:03 +0545

    review app models

--- foody/settings/base.py
+++ foody/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'restaurants',
     'foods',
+    'reviews',
 ]
 
 MIDDLEWARE = [
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,19 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+from restaurants.models import Restaurant
+from foods.models import Food
+
+class Review(models.Model):
+
+    restaurant      = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='rest')
+    food            = models.ForeignKey(Food, on_delete=models.CASCADE, related_name='foo')
+
+    rating          = models.SmallIntegerField(default=0, validators=[MaxValueValidator(5), MinValueValidator(0)])
+    comment_head    = models.CharField(max_length=50, null=True, blank=True)
+    comment_body    = models.CharField(max_length=150, null=True, blank=True)
+
+    def __str__(self):
+        return self.comment_head
+
+
